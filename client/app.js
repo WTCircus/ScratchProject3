8110374436f4a1c7b3303ee14ffe0802753ecfea
Spenser Schwartz <spensernet@gmail.com>
2020-07-27 17:48:19 -0700

    Push to pull

--- client/app.js
+++ client/app.js
@@ -1,8 +1,8 @@
 import React, { Fragment, Component } from 'react';
 //import React, { Component } from 'react';
 const ReactDOM = require('react-dom');
-import "@babel/polyfill";
-import axios from 'axios'
+import '@babel/polyfill';
+import axios from 'axios';
 
 // importing different pages for routing
 import CompanyProfile from './src/layout/CompanyProfile';
@@ -11,18 +11,16 @@ import Login from './src/layout/Login';
 import Register from './src/layout/Register';
 import Settings from './src/layout/Settings';
 import './styles/styles.scss';
- 
-
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
 
 let setData = []; // stores the user data in async call on line 92
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { // setting the default state
+    this.state = {
+      // setting the default state
       name: '',
       email: this.props.email,
       depthLevel: 0,
@@ -39,21 +37,11 @@ class App extends Component {
     };
     this.onSearchClick = this.onSearchClick.bind(this);
     this.onSliderChange = this.onSliderChange.bind(this);
-    this.getUser = this.getUser.bind(this)
+    this.getUser = this.getUser.bind(this);
   }
-
-  // ! setting the state
-  componentDidMount(){
-    const data = this.getUser();
-    console.log("inside did mount" + data)
-    //this.getUser()
-      // create function that will set the state. Makes Async Call
-    this.setState({companyListArray: setData}) // Set initial state 
-  }
-
 
   render() {
-    console.log(this.state) // test to console.log the state
+    console.log(this.state); // test to console.log the state
     return (
       <Router>
         <Route
@@ -88,27 +76,42 @@ class App extends Component {
       return;
     }
     output.push(e.target.company.value);
-    this.setState({ companyListArray: output }); // setting the state
+    // fetch (`/company/search?companyname=${e.target.company.value}`)
+    fetch(
+      `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${e.target.company.value}&apikey=R4TK10Z7D2U5QEA6`
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log('Search Results Data: ', data);
+        this.setState({ searchResults: data });
+      });
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    console.log('This is prevProps:  ', prevProps);
+    console.log('this.state in componenetdidupdate:  ', this.state);
+    console.log('This is prevState:   ', prevState);
+  }
 
   // function to delete cards from the array
 
   // function makes async call to database to get specific user data
-  async getUser(email){
-    if (email){
-      const data = await axios.get('http://localhost:3000/api/users/' + email) // makes axios request to get specific user
-      setData = data.data.favorites
-      this.setState({companyListArray: setData})
-    } else{
-      setData=["GOOG","FB","AMZN"] // default data
-      this.setState({companyListArray: setData})
+  async getUser(email) {
+    if (email) {
+      const data = await axios.get('http://localhost:3000/api/users/' + email); // makes axios request to get specific user
+      setData = data.data.favorites;
+      this.setState({ companyListArray: setData });
+    } else {
+      setData = ['GOOG', 'FB', 'AMZN']; // default data
+      this.setState({ companyListArray: setData });
     }
   }
 
   onSliderChange(number) {
-    this.setState({depthLevel : number});
-  };
+    this.setState({ depthLevel: number });
+  }
 }
 
 export default App;
